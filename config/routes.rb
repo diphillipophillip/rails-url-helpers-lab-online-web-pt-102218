9812b03b8857414f47d9402008b083dae0fc957b
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-03-19 01:55:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   
   
 resources :students, only: [:index, :show, :activate]
-  
+   get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
